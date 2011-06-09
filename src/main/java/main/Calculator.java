9f4c2dfd09a1e9cecb48dccaa0e08e776c3787b9
Michel Menegazzo <michelmenega@gmail.com>
2011-06-09 00:48:46 -0300

    Deletando projeto antigo

--- src/main/java/main/Calculator.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public interface Calculator {
-
-	public String add(String left, String right);
-	public String subtract(String left, String right);
-	public int compare(String left, String right);
-		
-}
